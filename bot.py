67026bb023a1256e95a91bc80cddc885c0020c44
Alfaz <muhammedalfaz2011@gmail.com>
2026-03-16 15:45:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ CHAT_ID = 7463999147
 
 bot = telegram.Bot(token=TOKEN)
 
-url = https://www.amazon.in/Ant-Esports-MK1300-Mini-Mechanical/dp/B09GG2YVWH/ref=sr_1_7?crid=39A5VJGSRYE5E&dib=eyJ2IjoiMSJ9.DPDadPwftyQZoNEiixRRqKXx7ES8AVGqR-0QPLRk8ccIx-4bgkhaqWa6AlnIlOZ8TV6fHeo-HYEwYwyrFTGIBh8nq5tNUZt-BZpU6zYx5af2VGFUiGIFLuOWZCGBrzYcJ2PdmbPvedNZ8UVn8a5Z9TmfLGk5B92m7S6BsjBO-mcJ0PTPDwwuELdQbZOO8ff9b5jjOVN40Qv0EFkTyPXmC_MoYFDP826QrzhXKSyNcYI.Yb5Zk4LQv4WqcprqSkHJg5dhrKXVUpyKlSO1o0IUlDQ&dib_tag=se&keywords=ant+esports+mechanical+keyboard&nsdOptOutParam=true&qid=1773654958&sprefix=ant+%2Caps%2C465&sr=8-7
+url = "https://www.amazon.in/Ant-Esports-MK1300-Mini-Mechanical/dp/B09GG2YVWH/ref=sr_1_7?crid=39A5VJGSRYE5E&dib=eyJ2IjoiMSJ9.DPDadPwftyQZoNEiixRRqKXx7ES8AVGqR-0QPLRk8ccIx-4bgkhaqWa6AlnIlOZ8TV6fHeo-HYEwYwyrFTGIBh8nq5tNUZt-BZpU6zYx5af2VGFUiGIFLuOWZCGBrzYcJ2PdmbPvedNZ8UVn8a5Z9TmfLGk5B92m7S6BsjBO-mcJ0PTPDwwuELdQbZOO8ff9b5jjOVN40Qv0EFkTyPXmC_MoYFDP826QrzhXKSyNcYI.Yb5Zk4LQv4WqcprqSkHJg5dhrKXVUpyKlSO1o0IUlDQ&dib_tag=se&keywords=ant+esports+mechanical+keyboard&nsdOptOutParam=true&qid=1773654958&sprefix=ant+%2Caps%2C465&sr=8-7"
 
 def send_price():
 
