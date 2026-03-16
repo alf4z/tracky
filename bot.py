3a0828a7cc3a2239d5ad4fc16fb8c1c5c9b8d539
Alfaz <muhammedalfaz2011@gmail.com>
2026-03-16 15:30:31 +0530

    Refactor bot to send daily price updates

--- bot.py
+++ bot.py
@@ -2,18 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 import schedule
 import time
-from telegram import Bot
+import telegram
 
-TOKEN = "YOUR_BOT_TOKEN"
-CHAT_ID = "YOUR_CHAT_ID"
+TOKEN = 8632230189:AAFF2bXQxoGV3bnj9hr1fzFbpvOYisVEE5w
+CHAT_ID = 7463999147
 
-bot = Bot(token=TOKEN)
+bot = telegram.Bot(token=TOKEN)
 
-url = "PRODUCT_LINK"
+url = https://www.amazon.in/Ant-Esports-MK1300-Mini-Mechanical/dp/B09GG2YVWH/ref=sr_1_7?crid=39A5VJGSRYE5E&dib=eyJ2IjoiMSJ9.DPDadPwftyQZoNEiixRRqKXx7ES8AVGqR-0QPLRk8ccIx-4bgkhaqWa6AlnIlOZ8TV6fHeo-HYEwYwyrFTGIBh8nq5tNUZt-BZpU6zYx5af2VGFUiGIFLuOWZCGBrzYcJ2PdmbPvedNZ8UVn8a5Z9TmfLGk5B92m7S6BsjBO-mcJ0PTPDwwuELdQbZOO8ff9b5jjOVN40Qv0EFkTyPXmC_MoYFDP826QrzhXKSyNcYI.Yb5Zk4LQv4WqcprqSkHJg5dhrKXVUpyKlSO1o0IUlDQ&dib_tag=se&keywords=ant+esports+mechanical+keyboard&nsdOptOutParam=true&qid=1773654958&sprefix=ant+%2Caps%2C465&sr=8-7
 
-target_price = 20000
-
-def check_price():
+def send_price():
 
     headers = {"User-Agent": "Mozilla/5.0"}
 
@@ -21,13 +19,22 @@ def check_price():
     soup = BeautifulSoup(page.content, "html.parser")
 
     price = soup.find("span", {"class": "a-price-whole"}).text
-    price = int(price.replace(",", ""))
 
-    if price < target_price:
-        bot.send_message(chat_id=CHAT_ID,
-                         text=f"🔥 Price dropped! Current price ₹{price}")
+    message = f""""
+    🍥Hey Alfaz,
+    
+📦 Product Price Update
 
-schedule.every(6).hours.do(check_price)
+Current Price: ₹{price}
+
+Checked Automatically Today
+""""
+
+    bot.send_message(chat_id=CHAT_ID, text=message)
+
+
+# run once per day
+schedule.every().day.at("09:00").do(send_price)
 
 while True:
     schedule.run_pending()
