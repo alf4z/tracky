3fd1f30f2385c7a6dea06aac84e6ceab54018bdf
Alfaz <muhammedalfaz2011@gmail.com>
2026-03-16 18:01:24 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,25 +4,32 @@ import schedule
 import time
 import telegram
 
-TOKEN = "8632230189:AAFF2bXQxoGV3bnj9hr1fzFbpvOYisVEE5w"
-CHAT_ID = "7463999147"
+TOKEN = "8632230189:AAE8ViNDOosEhHSfWC1582q868vJYNEQcJQ"
+CHAT_ID = "468978056"
 
 bot = telegram.Bot(token=TOKEN)
 
-url = "https://www.amazon.in/Ant-Esports-MK1300-Mini-Mechanical/dp/B09GG2YVWH/ref=sr_1_7?crid=39A5VJGSRYE5E&dib=eyJ2IjoiMSJ9.DPDadPwftyQZoNEiixRRqKXx7ES8AVGqR-0QPLRk8ccIx-4bgkhaqWa6AlnIlOZ8TV6fHeo-HYEwYwyrFTGIBh8nq5tNUZt-BZpU6zYx5af2VGFUiGIFLuOWZCGBrzYcJ2PdmbPvedNZ8UVn8a5Z9TmfLGk5B92m7S6BsjBO-mcJ0PTPDwwuELdQbZOO8ff9b5jjOVN40Qv0EFkTyPXmC_MoYFDP826QrzhXKSyNcYI.Yb5Zk4LQv4WqcprqSkHJg5dhrKXVUpyKlSO1o0IUlDQ&dib_tag=se&keywords=ant+esports+mechanical+keyboard&nsdOptOutParam=true&qid=1773654958&sprefix=ant+%2Caps%2C465&sr=8-7"
+url = "https://www.amazon.in/Ant-Esports-MK1300-Mini-Mechanical/dp/B09GG2YVWH"
 
 def send_price():
+
+    print("Checking price...")
 
     headers = {"User-Agent": "Mozilla/5.0"}
 
     page = requests.get(url, headers=headers)
     soup = BeautifulSoup(page.content, "html.parser")
 
-    price = soup.find("span", {"class": "a-price-whole"}).text
+    price_tag = soup.find("span", {"class": "a-price-whole"})
+
+    if price_tag:
+        price = price_tag.text.strip()
+    else:
+        price = "Price not found"
 
     message = f"""
-    🍥Hey Alfaz,
-    
+🍥 Hey Alfaz,
+
 📦 Product Price Update
 
 Current Price: ₹{price}
@@ -32,10 +39,16 @@ Checked Automatically Today
 
     bot.send_message(chat_id=CHAT_ID, text=message)
 
+    print("Message sent!")
 
-# run once per day
+# run once immediately for testing
+send_price()
+
+print("Bot started...")
+
 schedule.every(1).minutes.do(send_price)
 
 while True:
+    print("Checking schedule...")
     schedule.run_pending()
-    time.sleep(60)
+    time.sleep(10)
